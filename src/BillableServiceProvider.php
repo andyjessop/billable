c7482f5c694c61cc0cdfae310e216797c52ff88f
Andy Jessopm <andyjessop.olp@gmail.com>
2015-06-13 11:13:22 +0100

    Initial commit

---
+++ src/BillableServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace AndyJessop\Billable;
+
+use Illuminate\Support\ServiceProvider;
+
+class BillableServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
---
+++ src/BillableTrait.php
@@ -0,0 +1,9 @@
+<?php namespace AndyJessop\Billable;
+
+/**
+ * Class BillableTrait
+ * @package AndyJessop\Billable
+ */
+trait BillableTrait {
+	
+}
